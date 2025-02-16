b6a677c077a976d4fb1080f7567bed163879afb1
1poundSOCKS <mathew.coburn@gmail.com>
2025-02-16 09:21:13 +0000

    runs a simple lua script

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,23 @@
+// #include "pch.h"
+#include <iostream>
+#include <lua.hpp>
+
 int main(int argc, char* argv[])
 {
   argc;
   argv;
+
+  lua_State* L;
+  L = luaL_newstate();
+  
+  lua_pushstring(L, "Anna");
+  lua_setglobal(L, "name");
+  luaL_openlibs(L);
+  if(luaL_dofile(L, "scripts/hello.lua")) {
+      std::cout << "Final:" << lua_tostring(L, -1) << "\n";
+  }
+  
+  lua_close(L);
+
   return 0;
 }
